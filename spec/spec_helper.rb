a3ef6c1a79de587fb644f9cd83fb30a87ee6ed69
ydah <t.yudai92@gmail.com>
2025-08-21 23:38:25 +0900

    Suppress the log output during execution rspec
    ```
    ❯ bundle exec rspec spec/lib/rspec/rebound_spec.rb
    .
    1st Try error in ./spec/lib/rspec/rebound_spec.rb:47:
    
    expected true
         got false
    
    RSpec::Rebound: 2nd try ./spec/lib/rspec/rebound_spec.rb:47
    
    2nd Try error in ./spec/lib/rspec/rebound_spec.rb:47:
    
    expected true
         got false
    
    RSpec::Rebound: 3rd try ./spec/lib/rspec/rebound_spec.rb:47
    .
    1st Try error in ./spec/lib/rspec/rebound_spec.rb:56:
    
    expected true
         got false
    
    RSpec::Rebound: 2nd try ./spec/lib/rspec/rebound_spec.rb:56
    .
    1st Try error in ./spec/lib/rspec/rebound_spec.rb:65:
    
    expected true
         got false
    
    RSpec::Rebound: 2nd try ./spec/lib/rspec/rebound_spec.rb:65
    ..
    1st Try error in ./spec/lib/rspec/rebound_spec.rb:95:
    
    expected true
         got false
    
    RSpec::Rebound: 2nd try ./spec/lib/rspec/rebound_spec.rb:95
    
    2nd Try error in ./spec/lib/rspec/rebound_spec.rb:95:
    
    expected true
         got false
    
    RSpec::Rebound: 3rd try ./spec/lib/rspec/rebound_spec.rb:95
    .
    1st Try error in ./spec/lib/rspec/rebound_spec.rb:107:
    
    expected true
         got false
    
    RSpec::Rebound: 2nd try ./spec/lib/rspec/rebound_spec.rb:107
    
    2nd Try error in ./spec/lib/rspec/rebound_spec.rb:107:
    
    expected true
         got false
    
    RSpec::Rebound: 3rd try ./spec/lib/rspec/rebound_spec.rb:107
    .**
    1st Try error in ./spec/lib/rspec/rebound_spec.rb:132:
    OtherError
    
    RSpec::Rebound: 2nd try ./spec/lib/rspec/rebound_spec.rb:132
    ...
    1st Try error in ./spec/lib/rspec/rebound_spec.rb:172:
    RetryError
    
    RSpec::Rebound: 2nd try ./spec/lib/rspec/rebound_spec.rb:172
    .
    1st Try error in ./spec/lib/rspec/rebound_spec.rb:179:
    RetryChildError
    
    RSpec::Rebound: 2nd try ./spec/lib/rspec/rebound_spec.rb:179
    ..
    1st Try error in ./spec/lib/rspec/rebound_spec.rb:199:
    Rescue me!
    
    RSpec::Rebound: 2nd try ./spec/lib/rspec/rebound_spec.rb:199
    .**
    1st Try error in ./spec/lib/rspec/rebound_spec.rb:224:
    RetryError
    
    RSpec::Rebound: 2nd try ./spec/lib/rspec/rebound_spec.rb:224
    .*
    1st Try error in ./spec/lib/rspec/rebound_spec.rb:251:
    
    expected false
         got true
    
    RSpec::Rebound: 2nd try ./spec/lib/rspec/rebound_spec.rb:251
    .
    1st Try error in ./spec/lib/rspec/rebound_spec.rb:255:
    
    expected false
         got true
    
    RSpec::Rebound: 2nd try ./spec/lib/rspec/rebound_spec.rb:255
    ...
    1st Try error in ./spec/lib/rspec/rebound_spec.rb:293:
    let's retry once!
    
    RSpec::Rebound: 2nd try ./spec/lib/rspec/rebound_spec.rb:293
    .....
    ```

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,6 @@ if Gem::Version.new(RUBY_VERSION) > Gem::Version.new('3')
 end
 
 RSpec.configure do |config|
-  config.verbose_retry = true
-  config.display_try_failure_messages = true
-
   config.around do |ex|
     RSpec::Core::Sandbox.sandboxed do |config|
       RSpec::Rebound.setup
